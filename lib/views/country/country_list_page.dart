c656ec01d711b3e435b72d69cb0b4d81d17cb744
Tee Yu June <56582497+yujune@users.noreply.github.com>
2023-06-08 18:30:58 +0800

    feat: Support search feature in country list page

--- lib/views/country/country_list_page.dart
+++ lib/views/country/country_list_page.dart
@@ -1,5 +1,7 @@
+import 'dart:async';
 import 'package:country/models/country.dart';
 import 'package:country/viewmodels/country_view_model.dart';
+import 'package:country/widgets/common/search_bar.dart';
 import 'package:country/widgets/country/country_list_item.dart';
 import 'package:country/widgets/loading/loading_indicator.dart';
 import 'package:flutter/material.dart';
@@ -17,9 +19,16 @@ class _CountryListPageState extends State<CountryListPage> {
   late CountryViewModel _viewModel;
   List<Country>? _countryList;
   bool _isLoading = true;
+  bool _isFiltering = false;
+  Timer? _searchDebounce;
+  TextEditingController searchEditingController = TextEditingController();
 
   void setIsLoading({bool loading = true}) => setState(
         () => _isLoading = loading,
+      );
+
+  void setIsFiltering({bool filtering = true}) => setState(
+        () => _isFiltering = filtering,
       );
 
   Future<void> onRefresh() async {
@@ -28,6 +37,27 @@ class _CountryListPageState extends State<CountryListPage> {
 
   void onCountryTap(String name) {
     context.go('/details/$name');
+  }
+
+  Future<void> onSearchTextChanged(String text) async {
+    if (_searchDebounce?.isActive ?? false) _searchDebounce?.cancel();
+    _searchDebounce = Timer(const Duration(milliseconds: 500), () async {
+      List<Country> filteredCountry = [];
+      setIsFiltering();
+      try {
+        if (text.isEmpty) {
+          filteredCountry = await _viewModel.getCountryList();
+        } else {
+          filteredCountry = await _viewModel.getCountry(name: text);
+        }
+      } catch (error) {
+        filteredCountry = [];
+      }
+      setState(() {
+        _countryList = filteredCountry;
+      });
+      setIsFiltering(filtering: false);
+    });
   }
 
   @override
@@ -42,18 +72,41 @@ class _CountryListPageState extends State<CountryListPage> {
   }
 
   @override
+  void dispose() {
+    searchEditingController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     if (_isLoading) return const LoadingIndicator();
-
     return Scaffold(
-      body: RefreshIndicator(
-        onRefresh: onRefresh,
-        child: ListView.builder(
-          itemCount: _countryList?.length ?? 0,
-          itemBuilder: (context, index) => CountryListItem(
-            name: _countryList?[index].name?.common ?? '',
-            flagImageUrl: _countryList?[index].flags?.png ?? '',
-            onTap: onCountryTap,
+      body: SafeArea(
+        child: RefreshIndicator(
+          onRefresh: onRefresh,
+          child: Column(
+            children: [
+              SearchBar(
+                margin: const EdgeInsets.all(10),
+                controller: searchEditingController,
+                onChanged: onSearchTextChanged,
+                onClearText: () {
+                  onSearchTextChanged('');
+                },
+              ),
+              Expanded(
+                child: _isFiltering
+                    ? const LoadingIndicator()
+                    : ListView.builder(
+                        itemCount: _countryList?.length ?? 0,
+                        itemBuilder: (context, index) => CountryListItem(
+                          name: _countryList?[index].name?.common ?? '',
+                          flagImageUrl: _countryList?[index].flags?.png ?? '',
+                          onTap: onCountryTap,
+                        ),
+                      ),
+              ),
+            ],
           ),
         ),
       ),
